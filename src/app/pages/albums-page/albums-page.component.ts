8f625af45315bc10566e74551d16ef1f68ca07dd
mhdelavega <huertadelavegamiguel@gmail.com>
2021-06-22 20:10:54 +0200

    FEAT: gallery styles

--- src/app/pages/albums-page/albums-page.component.ts
+++ src/app/pages/albums-page/albums-page.component.ts
@@ -1,6 +1,8 @@
+import { ArtistsService } from './../../shared/services/artists/artists.service';
 import { AlbumInterface } from './../../core/models/AlbumInterface';
 import { Component, OnInit } from '@angular/core';
 import { AlbumsService } from 'src/app/shared/services/albums/albums.service';
+import { ArtistInterface } from 'src/app/core/models/ArtistInterface';
 
 @Component({
   selector: 'app-albums-page',
@@ -10,12 +12,15 @@ import { AlbumsService } from 'src/app/shared/services/albums/albums.service';
 export class AlbumsPageComponent implements OnInit {
 
   albumList: AlbumInterface[] = []; 
+  artistList: ArtistInterface[] = []; 
 
-  constructor(private albumsService: AlbumsService) { }
+  constructor(private albumsService: AlbumsService, private artistsService: ArtistsService) { }
 
   ngOnInit(): void {
     this.albumsService.getAlbums()
       .subscribe((data: any)=>{this.albumList = data;});
+    this.artistsService.getArtists()
+      .subscribe((data: any)=>{this.artistList = data;});
   } 
 
 }

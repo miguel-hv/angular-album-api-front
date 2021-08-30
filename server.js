f3640c51d58dfc9211c8012d163ea58cfe368fa5
mhdelavega <huertadelavegamiguel@gmail.com>
2021-08-30 15:59:15 +0200

    FEAT: heroku deploy

--- server.js
+++ server.js
@@ -6,6 +6,6 @@ const app = express();
 app.use(express.static(__dirname + '/dist/dev-challenge-app'));
 
 app.get('/*', (req, res) => 
-    res.sendFile(path.join(__dirname + 'dist/dev-challenge-app/index.html')));
+    res.sendFile(path.join(__dirname + '/dist/dev-challenge-app/index.html')));
 
 app.listen(process.env.PORT || 5000);
